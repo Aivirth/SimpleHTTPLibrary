4f4292e8d1546074ea9f92d2daa4b9f6b8edc9f5
Aivirth <aivirth.work@gmail.com>
2018-02-10 18:34:40 +0100

    Added callback to ajax request and error report

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 "use strict";
 
-(function(){
+    const http = new easyHTTP;
+    http.get('https://jsonplaceholder.typicode.com/posts', function(error ,response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response);
+        }
+        
+        
+    });
 
-});
